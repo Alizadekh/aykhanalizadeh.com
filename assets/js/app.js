b290de929b2527594838fb75ac4bae87a8dda8ca
Alizadekh <aykhanalizadekh@gmail.com>
2023-03-24 22:33:44 +0400

    EducationComplete

--- assets/js/app.js
+++ assets/js/app.js
@@ -64,3 +64,34 @@ function incrementthankCount() {
     thankCountEl.textContent = `${thankCount}`;
 }
 plus3El.addEventListener('click', incrementthankCount);
+
+
+
+const degreeTitle = document.querySelector('.degree-title');
+const infoBox = document.querySelector('.info-box');
+const toggleBtn = document.querySelector('#toggle-info');
+
+toggleBtn.addEventListener('click', () => {
+  infoBox.classList.toggle('show');
+});
+
+degreeTitle.addEventListener('click', (event) => {
+  if (!event.target.closest('#toggle-info')) {
+    infoBox.classList.remove('show');
+  }
+});
+
+const degree1Title = document.querySelector('.degree-title1');
+const infoBox1 = document.querySelector('.info-box1');
+const toggleBtn1 = document.querySelector('#toggle-info1');
+
+toggleBtn1.addEventListener('click', () => {
+  infoBox1.classList.toggle('show');
+});
+
+degree1Title.addEventListener('click', (event) => {
+  if (!event.target.closest('#toggle-info1')) {
+    infoBox.classList.remove('show');
+  }
+});
+
