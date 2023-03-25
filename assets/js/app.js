08dc46394ea9e256897d9616b9336b08ca33d1f8
Alizadekh <aykhanalizadekh@gmail.com>
2023-03-25 20:46:18 +0400

    RemoveAnimation

--- assets/js/app.js
+++ assets/js/app.js
@@ -105,29 +105,6 @@ function toggleNavbar() {
 }
 
 
-const sections = document.querySelectorAll('.right section');
-
-const isInViewport = (el) => {
-  const rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-};
-
-const runAnimation = () => {
-  sections.forEach((section) => {
-    if (isInViewport(section)) {
-      section.classList.add('animate-in');
-    }
-  });
-};
-
-window.addEventListener('scroll', () => {
-  runAnimation();
-});
 
 
 
