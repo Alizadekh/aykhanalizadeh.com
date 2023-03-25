ce570cdd62008e25da552b6bfc10b3bcf87798d8
Alizadekh <aykhanalizadekh@gmail.com>
2023-03-25 20:41:12 +0400

    AddAnimation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,3 @@
-
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -104,6 +103,31 @@ function toggleNavbar() {
     navbarContent.style.display = "block";
   }
 }
+
+
+const sections = document.querySelectorAll('.right section');
+
+const isInViewport = (el) => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const runAnimation = () => {
+  sections.forEach((section) => {
+    if (isInViewport(section)) {
+      section.classList.add('animate-in');
+    }
+  });
+};
+
+window.addEventListener('scroll', () => {
+  runAnimation();
+});
 
 
 
