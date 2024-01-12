17cc7d18d5e938b20d7e7fe6b779901411a4dc65
Alizadekh <aykhanalizadekh@gmail.com>
2024-01-13 00:40:16 +0400

    Fix some bugs

--- assets/back/app.py
+++ assets/back/app.py
@@ -12,19 +12,19 @@ def send_email():
     
     smtp_server = "aykhanalizadekh@gmail.com"
     smtp_port = 587
-    smtp_username = "example@gmail.com"  # E-poçt adresi
-    smtp_password = "password"  # E-poçt hesabının şifresi
+    smtp_username = "aykhanalizadekh@gmail.com"  # E-poçt adresi
+    smtp_password = "Ayxan20049115"  # E-poçt hesabının şifresi
     
-    # E-poçt məzmunu
+
     email_body = f"Name: {name}\nSurname: {surname}\nSubject: {subject}\nMessage: {message}"
     
     try:
-        # SMTP serverə bağlan
+        
         server = smtplib.SMTP(smtp_server, smtp_port)
         server.starttls()
         server.login(smtp_username, smtp_password)
         
-        # E-poçt göndər
+      
         server.sendmail(smtp_username, smtp_username, email_body)
         server.quit()
         
