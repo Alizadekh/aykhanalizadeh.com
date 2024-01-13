4f6ce55326fe13ef1b926d61db72fe5f0e607125
Alizadekh <aykhanalizadekh@gmail.com>
2024-01-13 12:46:58 +0400

    Fixing some bugs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,48 +1,38 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
+  });
 });
 
+const degreeTitle = document.querySelector(".degree-title");
+const infoBox = document.querySelector(".info-box");
+const toggleBtn = document.querySelector("#toggle-info");
 
-
-
-
-
-
-
-
-
-const degreeTitle = document.querySelector('.degree-title');
-const infoBox = document.querySelector('.info-box');
-const toggleBtn = document.querySelector('#toggle-info');
-
-toggleBtn.addEventListener('click', () => {
-  infoBox.classList.toggle('show');
+toggleBtn.addEventListener("click", () => {
+  infoBox.classList.toggle("show");
 });
 
-degreeTitle.addEventListener('click', (event) => {
-  if (!event.target.closest('#toggle-info')) {
-    infoBox.classList.remove('show');
+degreeTitle.addEventListener("click", (event) => {
+  if (!event.target.closest("#toggle-info")) {
+    infoBox.classList.remove("show");
   }
 });
 
+const degree1Title = document.querySelector(".degree-title1");
+const infoBox1 = document.querySelector(".info-box1");
+const toggleBtn1 = document.querySelector("#toggle-info1");
 
-const degree1Title = document.querySelector('.degree-title1');
-const infoBox1 = document.querySelector('.info-box1');
-const toggleBtn1 = document.querySelector('#toggle-info1');
-
-toggleBtn1.addEventListener('click', () => {
-  infoBox1.classList.toggle('show');
+toggleBtn1.addEventListener("click", () => {
+  infoBox1.classList.toggle("show");
 });
 
-degree1Title.addEventListener('click', (event) => {
-  if (!event.target.closest('#toggle-info1')) {
-    infoBox.classList.remove('show');
+degree1Title.addEventListener("click", (event) => {
+  if (!event.target.closest("#toggle-info1")) {
+    infoBox.classList.remove("show");
   }
 });
 
@@ -55,16 +45,15 @@ function toggleNavbar() {
   }
 }
 
-
 function animateNumber(el) {
   var start = 0,
-      end = parseInt(el.innerText),
-      duration = 1000, // animasyon süresi (ms)
-      range = end - start,
-      current = start,
-      increment = end > start ? 1 : -1,
-      stepTime = Math.abs(Math.floor(duration / range)),
-      obj = el;
+    end = parseInt(el.innerText),
+    duration = 1000,
+    range = end - start,
+    current = start,
+    increment = end > start ? 1 : -1,
+    stepTime = Math.abs(Math.floor(duration / range)),
+    obj = el;
 
   function animate() {
     var val = Math.round(current);
@@ -79,144 +68,157 @@ function animateNumber(el) {
   animate();
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  var coffee = document.querySelector('.coffee span'),
-      project = document.querySelector('.project span'),
-      certificates = document.querySelector('.certificates span'),
-      thank = document.querySelector('.thank span');
-  
-  window.addEventListener('scroll', function() {
+document.addEventListener("DOMContentLoaded", function (event) {
+  var coffee = document.querySelector(".coffee span"),
+    project = document.querySelector(".project span"),
+    certificates = document.querySelector(".certificates span"),
+    thank = document.querySelector(".thank span");
+
+  window.addEventListener("scroll", function () {
     var scrollPosition = window.innerHeight + window.scrollY;
-    
-    if (coffee.getBoundingClientRect().top < scrollPosition && coffee.innerText == '111') {
+
+    if (
+      coffee.getBoundingClientRect().top < scrollPosition &&
+      coffee.innerText == "111"
+    ) {
       animateNumber(coffee);
     }
-    if (project.getBoundingClientRect().top < scrollPosition && project.innerText == '15') {
+    if (
+      project.getBoundingClientRect().top < scrollPosition &&
+      project.innerText == "15"
+    ) {
       animateNumber(project);
     }
-    if (certificates.getBoundingClientRect().top < scrollPosition && certificates.innerText == '6') {
+    if (
+      certificates.getBoundingClientRect().top < scrollPosition &&
+      certificates.innerText == "6"
+    ) {
       animateNumber(certificates);
     }
-    if (thank.getBoundingClientRect().top < scrollPosition && thank.innerText == '3') {
+    if (
+      thank.getBoundingClientRect().top < scrollPosition &&
+      thank.innerText == "3"
+    ) {
       animateNumber(thank);
     }
   });
 });
 
-
-
 function switchThemeToBlack() {
-  document.getElementById("sunicon").style.backgroundColor = "transparent"
-  document.getElementById("sunicon").style.transition = "background-color 0.5s"
-  document.getElementById("moonicon").style.backgroundColor = "rgb(167, 167, 167)"
-
-
-
+  document.getElementById("sunicon").style.backgroundColor = "transparent";
+  document.getElementById("sunicon").style.transition = "background-color 0.5s";
+  document.getElementById("moonicon").style.backgroundColor =
+    "rgb(167, 167, 167)";
 
   document.getElementsByTagName("body")[0].style.backgroundColor = "black";
-  document.getElementsByTagName("body")[0].style.transition = "background-color 0.5s"
-  document.getElementById("left-part").style.backgroundColor = "#393646"
-  document.getElementById("name").style.color = "white"
-  document.getElementById("profession").style.color = "white"
-  
-  let navbarLinks = document.getElementById("navbar").querySelectorAll("a")
+  document.getElementsByTagName("body")[0].style.transition =
+    "background-color 0.5s";
+  document.getElementById("left-part").style.backgroundColor = "#393646";
+  document.getElementById("name").style.color = "white";
+  document.getElementById("profession").style.color = "white";
+
+  let navbarLinks = document.getElementById("navbar").querySelectorAll("a");
   for (let i = 0; i < navbarLinks.length; i++) {
-    navbarLinks[i].style.color = "white"
+    navbarLinks[i].style.color = "white";
   }
-  document.getElementById("footer").querySelector("p").style.color = "white"
-  document.getElementById("home").style.color =  "white"
-  document.getElementById("home").querySelector("button").style.color = "white"
-  document.getElementById("home").querySelector("button").style.borderColor = "white"
-  document.getElementById("home").querySelector("button").addEventListener("mouseover", function() {
-    this.style.backgroundColor = "white";
-    this.style.color = "black"
-  document.getElementById("home").querySelector("button").addEventListener("mouseout", function() {
-    this.style.backgroundColor = "";
-    this.style.color = "white"
+  document.getElementById("footer").querySelector("p").style.color = "white";
+  document.getElementById("home").style.color = "white";
+  document.getElementById("home").querySelector("button").style.color = "white";
+  document.getElementById("home").querySelector("button").style.borderColor =
+    "white";
+  document
+    .getElementById("home")
+    .querySelector("button")
+    .addEventListener("mouseover", function () {
+      this.style.backgroundColor = "white";
+      this.style.color = "black";
+      document
+        .getElementById("home")
+        .querySelector("button")
+        .addEventListener("mouseout", function () {
+          this.style.backgroundColor = "";
+          this.style.color = "white";
+        });
     });
-  });
 
+  let abouttags = document.getElementById("about").querySelector("h3");
+  abouttags.style.color = "white";
 
-  let abouttags = document.getElementById("about").querySelector("h3")
-  abouttags.style.color = "white"
-
-  let abouttagsp = document.getElementById("about").querySelectorAll("p")
+  let abouttagsp = document.getElementById("about").querySelectorAll("p");
   for (let i = 0; i < abouttagsp.length; i++) {
-    abouttagsp[i].style.color = "white"
+    abouttagsp[i].style.color = "white";
   }
-  document.getElementById("about").querySelector("button").style.color = "white"
-  document.getElementById("about").querySelector("button").style.borderColor = "white"
-  document.getElementById("about").querySelector("button").addEventListener("mouseover", function() {
-    this.style.backgroundColor = "white";
-    this.style.color = "black"
-  document.getElementById("about").querySelector("button").addEventListener("mouseout", function() {
-    this.style.backgroundColor = "";
-    this.style.color = "white"
+  document.getElementById("about").querySelector("button").style.color =
+    "white";
+  document.getElementById("about").querySelector("button").style.borderColor =
+    "white";
+  document
+    .getElementById("about")
+    .querySelector("button")
+    .addEventListener("mouseover", function () {
+      this.style.backgroundColor = "white";
+      this.style.color = "black";
+      document
+        .getElementById("about")
+        .querySelector("button")
+        .addEventListener("mouseout", function () {
+          this.style.backgroundColor = "";
+          this.style.color = "white";
+        });
     });
-  });
 
-  let boxes = document.getElementById("about-boxes").querySelectorAll("div")
+  let boxes = document.getElementById("about-boxes").querySelectorAll("div");
   for (i = 0; i < boxes.length; i++) {
-    boxes[i].style.backgroundColor = "#393646"
+    boxes[i].style.backgroundColor = "#393646";
   }
-  let boxtitles = document.getElementById("about-boxes").querySelectorAll("h3")
+  let boxtitles = document.getElementById("about-boxes").querySelectorAll("h3");
   for (i = 0; i < boxtitles.length; i++) {
-    boxtitles[i].style.color = "white"
+    boxtitles[i].style.color = "white";
   }
 
-
-
-  document.getElementById("skills").querySelector("h3").style.color = "White"
-  let skillsparagraph = document.getElementById("skills").querySelectorAll("p")
+  document.getElementById("skills").querySelector("h3").style.color = "White";
+  let skillsparagraph = document.getElementById("skills").querySelectorAll("p");
   for (i = 0; i < skillsparagraph.length; i++) {
-    skillsparagraph[i].style.color = "white"
+    skillsparagraph[i].style.color = "white";
   }
 
+  document.getElementById("edu-title").querySelector("p").style.color = "white";
+  document.getElementById("edu-title").querySelector("h3").style.color =
+    "white";
+  document.getElementById("info-box").querySelector("p").style.color = "white";
+  document.getElementById("info-box1").querySelector("p").style.color = "white";
 
-  document.getElementById("edu-title").querySelector("p").style.color = "white"
-  document.getElementById("edu-title").querySelector("h3").style.color = "white"
-  document.getElementById("info-box").querySelector("p").style.color = "white"
-  document.getElementById("info-box1").querySelector("p").style.color = "white"
-
-
-  const experience = document.getElementById("experience").querySelectorAll("div")
-  for (i =0 ; i < experience.length; i++) {
-    experience[i].style.color = "white"
+  const experience = document
+    .getElementById("experience")
+    .querySelectorAll("div");
+  for (i = 0; i < experience.length; i++) {
+    experience[i].style.color = "white";
   }
 
-
-  const contact = document.getElementById("contact").querySelectorAll("div")
-  for (i=0; i < contact.length; i++) {
-    contact[i].style.color = "white"
+  const contact = document.getElementById("contact").querySelectorAll("div");
+  for (i = 0; i < contact.length; i++) {
+    contact[i].style.color = "white";
   }
-  document.getElementById("contact").querySelector("a").style.color = "white"
+  document.getElementById("contact").querySelector("a").style.color = "white";
 
-  const inputs =  document.getElementById("contact-left").querySelectorAll("all-info")
+  const inputs = document
+    .getElementById("contact-left")
+    .querySelectorAll("all-info");
   for (i = 0; i < inputs.length; i++) {
-    const logo = inputs.querySelectorAll("c-logo")
+    const logo = inputs.querySelectorAll("c-logo");
     for (i = 0; i < logo.length; i++) {
-      logo[i].style.backgroundColor = "#393646"
+      logo[i].style.backgroundColor = "#393646";
     }
   }
-
-
-
-  
-
 }
-
-
-
-
-
-
 
 function switchThemeToWhite() {
   document.getElementById("sunicon").style.backgroundColor = "";
   document.getElementById("moonicon").style.backgroundColor = "";
 
   document.body.style.backgroundColor = "white";
-  document.getElementById("left-part").style.backgroundColor = "rgb(242, 243, 247)";
+  document.getElementById("left-part").style.backgroundColor =
+    "rgb(242, 243, 247)";
   document.getElementById("name").style.color = "black";
   document.getElementById("profession").style.color = "black";
 
@@ -227,16 +229,23 @@ function switchThemeToWhite() {
 
   document.getElementById("footer").querySelector("p").style.color = "black";
   document.getElementById("home").style.color = "black";
-  document.getElementById("home").querySelector("button").style.color = "black"
-  document.getElementById("home").querySelector("button").style.borderColor = "black"
-  document.getElementById("home").querySelector("button").addEventListener("mouseover", function() {
-    this.style.backgroundColor = "black";
-    this.style.color = "white"
-  document.getElementById("home").querySelector("button").addEventListener("mouseout", function() {
-    this.style.backgroundColor = "";
-    this.style.color = "black"
+  document.getElementById("home").querySelector("button").style.color = "black";
+  document.getElementById("home").querySelector("button").style.borderColor =
+    "black";
+  document
+    .getElementById("home")
+    .querySelector("button")
+    .addEventListener("mouseover", function () {
+      this.style.backgroundColor = "black";
+      this.style.color = "white";
+      document
+        .getElementById("home")
+        .querySelector("button")
+        .addEventListener("mouseout", function () {
+          this.style.backgroundColor = "";
+          this.style.color = "black";
+        });
     });
-  });
 
   let abouttags = document.getElementById("about").querySelector("h3");
   abouttags.style.color = "black";
@@ -246,24 +255,32 @@ function switchThemeToWhite() {
     abouttagsp[i].style.color = "black";
   }
 
-  document.getElementById("about").querySelector("button").style.color = "black"
-  document.getElementById("about").querySelector("button").style.borderColor = "black"
-  document.getElementById("about").querySelector("button").addEventListener("mouseover", function() {
-    this.style.backgroundColor = "black";
-    this.style.color = "white"
-  document.getElementById("about").querySelector("button").addEventListener("mouseout", function() {
-    this.style.backgroundColor = "";
-    this.style.color = "black"
+  document.getElementById("about").querySelector("button").style.color =
+    "black";
+  document.getElementById("about").querySelector("button").style.borderColor =
+    "black";
+  document
+    .getElementById("about")
+    .querySelector("button")
+    .addEventListener("mouseover", function () {
+      this.style.backgroundColor = "black";
+      this.style.color = "white";
+      document
+        .getElementById("about")
+        .querySelector("button")
+        .addEventListener("mouseout", function () {
+          this.style.backgroundColor = "";
+          this.style.color = "black";
+        });
     });
-  });
 
-  let boxes = document.getElementById("about-boxes").querySelectorAll("div")
+  let boxes = document.getElementById("about-boxes").querySelectorAll("div");
   for (i = 0; i < boxes.length; i++) {
-    boxes[i].style.backgroundColor = ""
+    boxes[i].style.backgroundColor = "";
   }
-  let boxtitles = document.getElementById("about-boxes").querySelectorAll("h3")
+  let boxtitles = document.getElementById("about-boxes").querySelectorAll("h3");
   for (i = 0; i < boxtitles.length; i++) {
-    boxtitles[i].style.color = "black"
+    boxtitles[i].style.color = "black";
   }
 
   document.getElementById("skills").querySelector("h3").style.color = "black";
@@ -277,7 +294,9 @@ function switchThemeToWhite() {
   document.getElementById("info-box").querySelector("p").style.color = "";
   document.getElementById("info-box1").querySelector("p").style.color = "";
 
-  const experience = document.getElementById("experience").querySelectorAll("div");
+  const experience = document
+    .getElementById("experience")
+    .querySelectorAll("div");
   for (let i = 0; i < experience.length; i++) {
     experience[i].style.color = "";
   }
@@ -288,7 +307,9 @@ function switchThemeToWhite() {
   }
   document.getElementById("contact").querySelector("a").style.color = "";
 
-  const inputs = document.getElementById("contact-left").querySelectorAll(".all-info");
+  const inputs = document
+    .getElementById("contact-left")
+    .querySelectorAll(".all-info");
   for (let i = 0; i < inputs.length; i++) {
     const logos = inputs[i].querySelectorAll(".c-logo");
     for (let j = 0; j < logos.length; j++) {
@@ -296,21 +317,3 @@ function switchThemeToWhite() {
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
